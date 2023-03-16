e4bb64fa9701d033a5712bbb12366b49f02cf618
shbone <598924626@qq.com>
2023-03-16 12:41:30 +0800

    edit  app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request
 from markupsafe import Markup
 import openai
@@ -5,16 +7,21 @@ import markdown
 import markdown.extensions.fenced_code
 import markdown.extensions.codehilite
 
-openai.api_key = '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'
+os.environ["http_proxy"] = "http://127.0.0.1:7890"
+os.environ["https_proxy"] = "http://127.0.0.1:7890"
+# openai.api_key = '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'
+# openai.api_key = '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'
+openai.api_key = "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"
 app = Flask(__name__)
 messages = []
+
+
 @app.route('/')
 def home():
-
     return render_template('index.html')
 
-@app.route('/get_response', methods=['POST'])
 
+@app.route('/get_response', methods=['POST'])
 def get_bot_response():
     user_input = request.form['user_input']
     # print(user_input)
@@ -23,15 +30,20 @@ def get_bot_response():
         model="gpt-3.5-turbo",
         messages=messages
     )
+
     ai_response = completion.choices[0].message['content']
     # print(ai_response)
     messages.append({'role': 'assistant', 'content': ai_response})
     print(messages)
-    return  Markup(markdown.markdown(ai_response, extensions=['fenced_code', 'codehilite']))
+    return Markup(markdown.markdown(ai_response, extensions=['fenced_code', 'codehilite']))
+
+
 @app.route('/reset')
 def reset():
     global messages
     messages = []
     return "Conversation history has been reset."
+
+
 if __name__ == '__main__':
     app.run(debug=True)
